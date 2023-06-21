245666f9a28a5a4a9cca1c4103644d5edf3d9671
Hojoong-Rome <hojoong9919@naver.com>
2023-06-21 17:00:18 +0900

    update

---
+++ src/main/java/com/mycompany/app/home/HomeController.java
@@ -0,0 +1,44 @@
+package com.mycompany.app.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value="/" )
+	public String home() {
+		// 여기에서 서버 단에서 필요한 작업을 수행한다.
+		
+		// href="/resources/xdm/v1/css/commonXdm.css"
+		
+		// 아래 jsp 파일 호출
+		return "home";
+	}
+	
+	@RequestMapping(value="/home2" )
+	public String home2() {
+		// 여기에서 서버 단에서 필요한 작업을 수행한다.
+		
+		
+		
+		// 아래 jsp 파일 호출
+		return "app/home/home2";
+	}
+	
+	@RequestMapping(value="/home3" )
+	public String home3() {
+		// 여기에서 서버 단에서 필요한 작업을 수행한다.
+		
+		
+		
+		// 아래 jsp 파일 호출
+		return "app/home/home3";
+	}
+	
+	@RequestMapping(value="/home4")
+	public String home4(){
+		return "app/home/home4";
+	}	
+	
+}
---
+++ src/main/webapp/WEB-INF/views/app/home/home3.jsp
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+		home3
+		</title>
+		<style>
+		
+		.good{
+			background-color: #333;
+			width: 400px;
+			height: 400px;
+			margin: auto;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+		.good p{
+			color: #fff;
+		}
+		
+		</style>
+	</head>
+	<body>
+		<div class="good"><p>haha</p></div>
+	</body>
+</html>
