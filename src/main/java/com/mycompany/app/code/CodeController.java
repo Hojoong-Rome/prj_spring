01496b0fc3993e591cac0e03409d9fd29ac110f0
Hojoong-Rome <hojoong9919@naver.com>
2023-06-26 09:17:11 +0900

    update

--- src/main/java/com/mycompany/app/code/CodeController.java
+++ src/main/java/com/mycompany/app/code/CodeController.java
@@ -1,5 +1,7 @@
 package com.mycompany.app.code;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +15,9 @@ public class CodeController {
 	
 	@RequestMapping("/codeList")
 	public String CodeList(Model model) {
-		model.addAttribute("list", service.selectList());
+		
+		List<Code> list = service.selectList();
+		model.addAttribute("list", list);
 		
 		return "codeList";
 	}
